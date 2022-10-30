8aec308cb963c29d58da62e424f6c5b3434a66d3
Tobias <tobiasnordahl@gmail.com>
2022-10-30 19:25:30 +0100

    Screen time and lights work

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,21 @@
 // https://github.com/olikraus/u8g2/wiki/setup_tutorial
 // https://www.aranacorp.com/en/using-an-oled-display-with-arduino/
 // https://github.com/olikraus/U8glib_Arduino/blob/master/examples/HelloWorld/HelloWorld.ino
+// https://efcomputer.net.au/blog/checkout/order-received/24145/?key=wc_order_qhEn46C4K2HWF + https://efcomputer.net.au/blog/product/esp8266-led-strip-wifi-source-code/ + https://www.instructables.com/WiFi-Controlled-RGB-LED-Strip-With-ESP8266/
 
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#include <NTPClient.h> // https://github.com/arduino-libraries/NTPClient
+#include <NTPClient.h>   // https://github.com/arduino-libraries/NTPClient
 #include <WiFiUdp.h>
 
 // OLED SCREEN
 #include <U8g2lib.h>
-//Parameters
-char cstr [16];//To convert int to char
-//Variables
-int oledCount  = 0;
+// Parameters
+char cstr[16]; // To convert int to char
+// Variables
+int oledCount = 0;
 
-//Objects
-// VIRKER 128x64: U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
+// Objects
+//  VIRKER 128x64: U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 // FORSØG MED 128x32.... VIRKER IKKE: U8X8_SSD1306_128X32_UNIVISION_2ND_HW_I2C u8x8(U8X8_PIN_NONE);
 // FORSØG MED 128x32.... VIRKER IKKE: U8X8_SSD1306_128X32_WINSTAR_2ND_HW_I2C u8x8(U8X8_PIN_NONE);
@@ -34,81 +35,109 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-
-// SCREEN
-//U8X8_SH1106_128X64_NONAME_HW_I2C u8g2(/* reset=*/ U8X8_PIN_NONE);
+// RGB Strip
+#include "Color.h"
+const int redLED = 14;   // D3 GPIO0
+const int greenLED = 12; // D2 GPIO2
+const int blueLED = 13;  // D4 GPIO4
+Color createColor(redLED, greenLED, blueLED);
 
 // SETUP
-void setup() {
-    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
+void setup()
+{
+  // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // it is a good practice to make sure your code sets wifi mode how you want it.
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wm;
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
-    // wm.resetSettings();
+  // WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
 
-    // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
+  // reset settings - wipe stored credentials for testing
+  // these are stored by the esp library
+  // wm.resetSettings();
 
-    bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP","standup"); // password protected ap
+  // Automatically connect using saved credentials,
+  // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+  // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+  // then goes into a blocking loop awaiting configuration and will return success result
 
-    if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi 
-        Serial.println("Connected...Wuhu :)");
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("AutoConnectAP", "standup"); // password protected ap
 
-        Serial.println("Calling timeClient");
-        timeClient.begin();
+  if (!res)
+  {
+    Serial.println("Failed to connect");
+    // ESP.restart();
+  }
+  else
+  {
+    // if you get here you have connected to the WiFi
+    Serial.println("Connected...Wuhu :)");
 
+    Serial.println("Calling timeClient");
+    timeClient.begin();
 
-        // Screen
-        // initialize with the I2C addr 0x3C
-        Serial.println("Screen begin");
-  // picture loop
-  u8x8.begin();
+    // Screen
+    // initialize with the I2C addr 0x3C
+    Serial.println("Screen begin");
+    // picture loop
+    u8x8.begin();
+    u8x8.setFont(u8x8_font_victoriabold8_r);
+
+    // Color test
+    createColor.off();
+    u8x8.clearDisplay();
+    u8x8.drawString(0, 0, "whiteRed");
+    createColor.whiteRed();
+    delay(1000);
+
+    createColor.off();
+    u8x8.clearDisplay();
+    u8x8.drawString(0, 0, "whiteGreen");
+    createColor.whiteGreen();
+    delay(1000);
+
+    createColor.off();
+    u8x8.clearDisplay();
+    u8x8.drawString(0, 0, "Blink animation");
+    createColor.blink();
+
+    u8x8.clearDisplay();
+    u8x8.drawString(0, 0, "Done, wait 2sec");
+    delay(2000);
+
+    createColor.off();
 
     Serial.println("Screen end");
-    }
+    u8x8.clearDisplay();
+  }
 }
 
-void loop() {
+void loop()
+{
   // u8x8.setFont(u8x8_font_chroma48medium8_r);
   // u8x8.drawString(0,0,"Hello World!");
   // delay(1000);
 
-    // Time
-    timeClient.update();
+  // Time
+  timeClient.update();
 
-    String formattedTime = timeClient.getFormattedTime();
-    // const char * c = formattedTime.c_str();
+  String formattedTime = timeClient.getFormattedTime();
+  // const char * c = formattedTime.c_str();
 
-    u8x8.setFont(u8x8_font_chroma48medium8_r);
-    u8x8.drawString(0,0,formattedTime.c_str()); // Txt needs to be "const char * c "... In String-type, that can be done with c_str()
-    delay(1000);
+  u8x8.drawString(0, 0, formattedTime.c_str()); // Txt needs to be "const char * c "... In String-type, that can be done with c_str()
+  delay(1000);
 
-    Serial.print(daysOfTheWeek[timeClient.getDay()]);
-    Serial.print(", ");
-    Serial.print(timeClient.getHours());
-    Serial.print(":");
-    Serial.print(timeClient.getMinutes());
-    Serial.print(":");
-    Serial.println(timeClient.getSeconds());
-    //Serial.println(timeClient.getFormattedTime());
-
-    delay(1000);
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  // Serial.println(timeClient.getFormattedTime());
 }
-
